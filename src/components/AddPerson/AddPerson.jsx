c61033ebfbbf7fb6a1fa47ae6f19174cf3b5b647
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-04-08 16:20:26 -0500

    add handleChange adn handleSubmit func

--- src/components/AddPerson/AddPerson.jsx
+++ src/components/AddPerson/AddPerson.jsx
@@ -2,14 +2,27 @@ import React, {Component} from 'react';
 
 class AddPerson extends Component {
     state = { 
-        teacherName: "" 
+        teacherName: "",
     }
+
+    handleChange = e => {
+        this.setState({
+          [e.target.name] : e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.handleAdd(this.state.teacherName, role);  {/*handleAdd is writen on app.js so need to pass handleAdd as a prop */}
+    }
+
     render() { 
         return (
             <form onSubmit={this.handleSubmit}>
                 <input 
-                    name="name"
+                    name="teacherName"
                     required
+                    onChange={this.handleChange}
                 />
                 <button type="submit">Submit</button>
             </form>
